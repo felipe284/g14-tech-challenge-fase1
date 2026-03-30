432188f424a600e96918ba7a2ebd4dcddac4a308
Jeferson Leal <jeferson.gleal@outlook.com>
2026-03-30 20:03:51 -0300

    :sparkles: feat: configuracoes iniciais do projeto

---
+++ foodlink-api/src/test/java/com/fiap_g14/foodlink/api/FoodlinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap_g14.foodlink.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodlinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
