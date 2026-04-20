9ea6c574792c7e257bbf6da0e098ed7325119989
Ailton Martins <pinho123@gmail.com>
2026-04-19 22:45:07 -0300

    feat(Usuario): Implementar funcionalidade de alterar a senha do usuários, criado a camada validação de regra de negocio, melhoria no tratamento de erros e criado a camada exception.

--- foodlink-api/src/main/java/com/fiap_g14/foodlink/api/controller/UserController.java
+++ foodlink-api/src/main/java/com/fiap_g14/foodlink/api/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.fiap_g14.foodlink.api.controller;
 
+import com.fiap_g14.foodlink.api.dto.ChangePasswordRequestDTO;
 import com.fiap_g14.foodlink.api.dto.UserResponseDTO;
 import com.fiap_g14.foodlink.api.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,7 +33,7 @@ public class UserController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Usuário excluído com sucesso"),
             @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
-            @ApiResponse(responseCode = "400", description = "ID deve ser um UUID válido")
+            @ApiResponse(responseCode = "400", description = "Erro de Regra de Negócio")
     })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -39,4 +41,16 @@ public class UserController {
         service.deleteUser(id);
     }
 
+    @Operation(summary = "Alterar a senha do usuário")
+    @ApiResponses(value ={
+        @ApiResponse(responseCode = "204", description = "Senha do usuário alterado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+        @ApiResponse(responseCode = "400", description = "Erro de Regra de Negócio"),
+        @ApiResponse(responseCode = "422", description = "Dados de entrada inválidos")
+    })
+    @PatchMapping("/change-password/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changePassword(@Validated @RequestBody ChangePasswordRequestDTO request, @PathVariable UUID id) {
+        service.changePassword(id, request);
+    }
 }
--- foodlink-api/src/main/java/com/fiap_g14/foodlink/api/service/UserService.java
+++ foodlink-api/src/main/java/com/fiap_g14/foodlink/api/service/UserService.java
@@ -1,8 +1,11 @@
 package com.fiap_g14.foodlink.api.service;
 
+import com.fiap_g14.foodlink.api.domain.UserEntity;
+import com.fiap_g14.foodlink.api.dto.ChangePasswordRequestDTO;
 import com.fiap_g14.foodlink.api.dto.UserResponseDTO;
 import com.fiap_g14.foodlink.api.mapper.UserMapper;
 import com.fiap_g14.foodlink.api.repository.UserRepository;
+import com.fiap_g14.foodlink.api.validator.UserValidator;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,7 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserValidator userValidator;
 
     public List<UserResponseDTO> getAllUsers() {
         return userRepository.findAll().stream().map(UserMapper::toDTO).toList();
@@ -25,5 +29,14 @@ public class UserService {
             throw new EntityNotFoundException("Usuário não encontrado");
         }
         userRepository.deleteById(id);
+    }
+
+    public void changePassword(UUID id, ChangePasswordRequestDTO changePasswordRequestDTO) {
+
+        UserEntity user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+        userValidator.validateChangePassword(user, changePasswordRequestDTO);
+
+        user.setSenha(changePasswordRequestDTO.getNovaSenha());
+        userRepository.save(user);
     }
 }
